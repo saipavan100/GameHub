5afd70f7d4f058d9df816a409bf8217de911a3ed
Yuanyuan Shen <77944820+AshleyShen101@users.noreply.github.com>
2021-12-15 02:40:35 -0800

    Update StoreItem

--- frontend/src/components/Gamer/StoreItem.js
+++ frontend/src/components/Gamer/StoreItem.js
@@ -29,29 +29,21 @@ const StoreItem = ({ game }) => {
     }
   };
   return (
-    <div className="card mb-30">
-      <div>
+    <div className="gamerStoreItemsContainer">
+      <div className="gamerStoreItemSection1">
         <div className="mainImg">
           <img src={game.gameImageURL} alt="Game Image" />
         </div>
+        <div className="gamerStoreItemTitle">{game.gameTitle}</div>
+        <div className="gamerStoreItemPublishedBy">{game.publishedBy}</div>
       </div>
-      <div className="card-body text-center">
-        <h4 className="card-title">{game.gameTitle}</h4>
-        <h5 className="card-text">
-          <small>price: </small>${game.gamePrice}
-        </h5>
-        <h5 className="card-text">
-          <small>published by: </small>
-          {game.publishedBy}
-        </h5>
-        <button
-          onClick={() => addHandler(game)}
-          className="btn btn-sm btn-warning float-right"
-        >
-          Add to Cart
-        </button>
-      </div>
+      <div className="gamerStoreItemPrice">{game.gamePrice}</div>
+      <div className="gamerStoreItemDescription">{game.gameDesc}</div>
+      <button onClick={() => addHandler(game)} className="btn btn-warning">
+        Add to Cart
+      </button>
     </div>
+
   );
 };
 
